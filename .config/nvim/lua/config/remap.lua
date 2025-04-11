07736498be3e51bc40c5fd6a07e0cdc123feb5c2
Robert Allen <rob@binarysunrise.dev>
2025-04-11 16:25:24 -0600

    Fix tmux navigation issues due to keybind conflicts. Fix lsp not showing calc or async-path completions

--- .config/nvim/lua/config/remap.lua
+++ .config/nvim/lua/config/remap.lua
@@ -29,15 +29,7 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C--bootstrap><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.config/nvim/lua/rob-not-bob/packer.lua<CR>")
-
 vim.keymap.set("n", "<leader>h", "<cmd>noh<CR>")
-
--- Vim keybindings for navigating splits --
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>")
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>")
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>")
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>")
 
 vim.keymap.set("n", "<C-u>", "<cmd>vertical resize -5<CR>")
 vim.keymap.set("n", "<C-i>", "<cmd>vertical resize +5<CR>")
